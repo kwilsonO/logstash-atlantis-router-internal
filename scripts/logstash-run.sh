60e7a1bc9e594534e885dd4526b4ea42a5d2005c
root <root@ip-10-144-206-29.ec2.internal>
2015-08-10 23:35:03 +0000

    made some basic bug fixes

--- scripts/logstash-run.sh
+++ scripts/logstash-run.sh
@@ -1,3 +1,3 @@
 rm /root/atlantis-analytics/logstash-atlantis-router-internal/logstash-1.5.3/out.log
 rm /root/atlantis-analytics/logstash-atlantis-router-internal/logstash-1.5.3/err.log
-/root/atlantis-analytics/logstash-atlantis-router-internal/logstash-1.5.3/bin/logstash --debug -f /root/atlantis-analytics/logstash-atlantis-router-internal/config-files/logstash-router-devbox.conf > out.log 2> err.log &
+/root/atlantis-analytics/logstash-atlantis-router-internal/logstash-1.5.3/bin/logstash --debug -f /root/atlantis-analytics/logstash-atlantis-router-internal/config-files/logstash-router-devbox.conf > logs/out.log 2> logs/err.log &
